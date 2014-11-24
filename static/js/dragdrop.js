941a393c74645e89245e5a4be9ab8c3a217ce2ad
muriloviana <murilo.vianamo@gmail.com>
2014-11-24 00:55:44 -0200

    DnD update

--- static/js/dragdrop.js
+++ static/js/dragdrop.js
@@ -1,67 +1,75 @@
-// Variável do diretório das imagens
-var static_url = '/static/images/'
-
-// Função que permite o drop dos elementos em uma determinada área (DnD)
-function allowDrop(ev) {
-	ev.preventDefault();
+function organize() {
+	var escopo = document.getElementById('escopo');
+	var vetor = escopo.getElementsByTagName('li');
+	for (var i = 1; i < vetor.length; i++) {
+		if (vetor[i].getElementsByTagName('img').length > 0){
+			var flag = false, j = i-1;
+			while (flag == false){
+				if (vetor[j].getElementsByTagName('img').length > 0) {
+					var bt_ant = vetor[j].getElementsByTagName('img')[0];
+					if (bt_ant.id == 'bt_for' || bt_ant.id == 'bt_while' || bt_ant.id == 'bt_if' || bt_ant.id == 'bt_else') {
+						var p = parseInt(vetor[j].style.paddingLeft.substring(0, vetor[j].style.paddingLeft.length - 2)) + 20;
+						vetor[i].style.paddingLeft = p.toString()+'px';
+						flag = true;
+					} else {
+						vetor[i].style.paddingLeft = vetor[j].style.paddingLeft;
+						j -= 1;
+						if (j <= 0) { flag=true; };
+					}
+				} else {
+					vetor[i].style.paddingLeft = '0';
+					flag = true;
+				}
+			}
+		}
+	}
 }
 
-// Função que arrasta o elemento (DnD)
-function drag(ev) {
-	ev.dataTransfer.setData('text', ev.target.id);
+var static_url = '/static/images/';
+var dragSrcEl = null;
+
+function handleDragStart(e) {
+
+	dragSrcEl = this;
+
+	e.dataTransfer.effectAllowed = 'move';
+	e.dataTransfer.setData('text/html', this.innerHTML);
 }
 
-// Função que solta o elemento (DnD)
-function drop(ev) {
-	ev.preventDefault();
-	var data = ev.dataTransfer.getData('text');
-	var original = document.getElementById(data);
-	var caixa = document.getElementById('cx_codigo');
-	var qimg = caixa.getElementsByTagName('img').length;
+function handleDragOver(e) {
+	if (e.preventDefault) {
+		e.preventDefault();
+	}
 
-	if (original.id == 'bt_bolo_chocolate' || original.id == 'bt_bolo_morango') {
+	e.dataTransfer.dropEffect = 'move';
 
-		var imgbolo = document.createElement('img');
-		imgbolo.src = original.src;
-		imgbolo.alt = original.alt;
-		if (qimg == 0) {
-			ev.target.appendChild(imgbolo);
-		};
-	
-	} else if (original.id == 'bt_for'){
-		
-		var imgfor = document.createElement('img'), imgend = document.createElement('img');
-		imgfor.src = static_url + 'bt_for.jpg';
-		imgend.src = static_url + 'bt_endfor.jpg';
-		imgfor.alt = imgend.alt = original.alt;
-		if (qimg == 0) {
-			ev.target.appendChild(imgfor);
-			ev.target.appendChild(imgend);
-		};
-	
-	} else if (original.id == 'bt_while'){
-
-		var imgwhile = document.createElement('img'), imgend = document.createElement('img');
-		imgwhile.src = static_url + 'bt_while.jpg';
-		imgend.src = static_url + 'bt_endwhile.jpg';
-		imgwhile.alt = imgend.alt = original.alt;
-		if (qimg == 0) {
-			ev.target.appendChild(imgwhile);
-			ev.target.appendChild(imgend);
-		};
-
-	} else if (original.id == 'bt_if'){
-
-		var imgif = document.createElement('img'), imgelse = document.createElement('img'), imgend = document.createElement('img');
-		imgif.src = static_url + 'bt_if.jpg';
-		imgelse.src = static_url + 'bt_else.jpg';
-		imgend.src = static_url + 'bt_endif.jpg';
-		imgif.alt = imgelse.alt = imgend.alt = original.alt;
-		if (qimg == 0) {
-			ev.target.appendChild(imgif);
-			ev.target.appendChild(imgelse);
-			ev.target.appendChild(imgend);
-		};
-
-	};
+	return false;
 }
+
+function handleDrop(e) {
+	if (e.stopPropagation) {
+		e.stopPropagation();
+	}
+
+	if (dragSrcEl != this) {
+		dragSrcEl.innerHTML = this.innerHTML;
+		this.innerHTML = e.dataTransfer.getData('text/html');
+	}
+
+	organize();
+
+	return false;
+}
+
+var cols = document.querySelectorAll('#box_comandos figure');
+[].forEach.call(cols, function(i) {
+	i.addEventListener('dragstart', handleDragStart, false);
+	i.addEventListener('dragover', handleDragOver, false);
+});
+
+var linhas = document.querySelectorAll('#escopo li');
+[].forEach.call(linhas, function(i) {
+	i.addEventListener('dragstart', handleDragStart, false);
+	i.addEventListener('dragover', handleDragOver, false);
+	i.addEventListener('drop', handleDrop, false);
+});
