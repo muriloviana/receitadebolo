9d90a759d4cf53aea4649966d2ac5b1194fcc228
muriloviana <murilo.vianamo@gmail.com>
2014-12-01 02:07:11 -0200

    Botão de ajuda.

--- static/js/ajuda.js
+++ static/js/ajuda.js
@@ -11,3 +11,35 @@ function ajudaOver(obj){
 function ajudaOut(obj){
 	obj.innerHTML = '';
 }
+
+//
+// Ajuda referente a fase.
+//
+function ajuda(fase){
+
+	if (fase == 1) {
+		alert('-> Clique, segure e arraste os blocos de comando para dentro do Escopo (área com a marca d\'água do jogo). Depois clique no botão "Executar".');
+	} else if (fase == 2){
+		alert('-> Clique, segure e arraste os blocos de comando para dentro do Escopo (área com a marca d\'água do jogo). Depois clique no botão "Executar".');
+	} else if (fase == 3){
+		alert('-> Não deixe linhas em branco entre os blocos de comando.\n-> Lembre-se de que o bloco "Repetir N vezes" só repete os blocos que estiverem abaixo dele.');
+	} else if (fase == 4){
+		alert('-> Clique, segure e arraste os blocos de comando para dentro do Escopo (área com a marca d\'água do jogo). Depois clique no botão "Executar".');
+	} else if (fase == 5){
+		alert('-> Não deixe linhas em branco entre os blocos de comando.\n-> Lembre-se de que o bloco "Repetir N vezes" só repete os blocos que estiverem abaixo dele.');
+	} else if (fase == 6){
+		alert('-> Não deixe linhas em branco entre os blocos de comando.\n-> Lembre-se de que o bloco "Repetir N vezes" só repete os blocos que estiverem abaixo dele.');
+	} else if (fase == 7){
+		alert('-> Leia com atenção o objetivo da fase.\n-> Não deixe linhas em branco entre os blocos de comando.\n-> Lembre-se de que o bloco "Enquanto houver pedido" só repete os blocos que estiverem abaixo dele.');
+	} else if (fase == 8){
+		alert('-> Leia com atenção o objetivo da fase.\n-> Não deixe linhas em branco entre os blocos de comando.\n-> Lembre-se de que o bloco "Enquanto houver pedido" só repete os blocos que estiverem abaixo dele.');
+	} else if (fase == 9){
+		alert('-> Leia com atenção o objetivo da fase.\n-> Evite deixar linhas em branco entre os blocos de comando.\n-> O bloco de comando "Senão" deve estar sempre em alguma linha após o comando "Se pedir ...", nunca antes.');
+	} else if (fase == 10){
+		alert('-> Leia com atenção o objetivo da fase.\n-> Não deixe linhas em branco entre os blocos de comando.\n-> O bloco de comando "Senão" deve estar sempre em alguma linha após o comando "Se pedir ...", nunca antes.\n-> Lembre-se de que o bloco "Repetir N vezes" só repete os blocos que estiverem abaixo dele.');
+	} else if (fase == 11){
+		alert('-> Leia com atenção o objetivo da fase.\n-> Não deixe linhas em branco entre os blocos de comando.\n-> O bloco de comando "Senão" deve estar sempre em alguma linha após o comando "Se pedir ...", nunca antes.\n-> Lembre-se de que o bloco "Enquanto houver pedido" só repete os blocos que estiverem abaixo dele.');
+	};
+
+	return false
+}
